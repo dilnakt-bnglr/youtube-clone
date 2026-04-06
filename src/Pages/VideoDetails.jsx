b2c874d41fa37c0eb281a4ae3809c30c7bee8e5a
dilnakt-bnglr <dilna.devaraj98@gmail.com>
2026-04-06 18:47:42 +0530

    integrated api for like and dislike

--- src/Pages/VideoDetails.jsx
+++ src/Pages/VideoDetails.jsx
@@ -7,12 +7,19 @@ import VideodetailItem from "../Components/VideDetails/VideodetailItem";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { AiFillLike } from "react-icons/ai";
+import { AiFillDislike } from "react-icons/ai";
 
 function VideoDetails() {
   const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const videoId = useParams().id;
   const [videoData, setVideoData] = useState("");
   const [comments, setComments] = useState([]);
+  const userId = JSON.parse(localStorage.getItem("userId")) || "";
+  const token = JSON.parse(localStorage.getItem("token")) || "";
+  const [isUserLiked, setIsUserLiked] = useState(false);
+  const [isUserDisliked, setIsUserDisliked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
 
   useEffect(() => {
     axios
@@ -25,6 +32,43 @@ function VideoDetails() {
         console.log(err);
       });
   }, [videoId]);
+
+  const handleLike = () => {
+    const bodyObject = { userId, videoId };
+    axios
+      .post("http://localhost:5000/api/user-action/like", bodyObject, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${token}`,
+        },
+      })
+      .then((response) => {
+        setIsUserLiked(response.data.isUserLiked);
+        const totalCount = response.data.totalLikes;
+        setLikeCount(totalCount?.length);
+        if (response.data.isUserDisliked !== undefined) {
+          setIsUserDisliked(response.data.isUserDisliked);
+        }
+      });
+  };
+
+  const handleDisLike = () => {
+    const bodyObject = { userId, videoId };
+    axios
+      .post("http://localhost:5000/api/user-action/dislike", bodyObject, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.data.isUserLiked !== undefined) {
+          setIsUserLiked(response.data.isUserLiked);
+        }
+        setIsUserDisliked(response.data.isUserDisliked);
+      });
+  };
+
   return (
     <div className="block sm:flex">
       <div className="p-2 mt-5 sm:w-[60%]">
@@ -50,15 +94,42 @@ function VideoDetails() {
             </div>
             <div className="flex gap-5">
               <div className="border-1 rounded-full p-2 text-2xl w-24 flex justify-between bg-gray-200 ">
-                <button>
-                  <BiLike />
-                </button>
-                <button>
-                  <BiDislike />
-                </button>
+                {isUserLiked ? (
+                  <div className="flex justify-center items-center">
+                    <span className="text-sm ">{likeCount}</span>
+                    <button
+                      className="cursor-pointer"
+                      onClick={() => handleLike()}
+                    >
+                      <AiFillLike />
+                    </button>
+                  </div>
+                ) : (
+                  <button
+                    className="cursor-pointer"
+                    onClick={() => handleLike()}
+                  >
+                    <BiLike />
+                  </button>
+                )}
+                {isUserDisliked ? (
+                  <button
+                    className="cursor-pointer"
+                    onClick={() => handleDisLike()}
+                  >
+                    <AiFillDislike />
+                  </button>
+                ) : (
+                  <button
+                    className="cursor-pointer"
+                    onClick={() => handleDisLike()}
+                  >
+                    <BiDislike />
+                  </button>
+                )}
               </div>
               <div className="border-1 rounded-full p-2 text-2xl w-24 text-center bg-gray-200">
-                <button>
+                <button className="cursor-pointer">
                   <PiShareFat />
                 </button>
               </div>
